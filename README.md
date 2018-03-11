6d47dec6df8c245a96fb6d86a3b3dcd2e24fd966
Jesse Brewer <jbrewerwriter@gmail.com>
2018-03-10 20:40:34 -0500

    html / css / js

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+![Banner Pic](https://github.com/jessebrewer988/dragon-quest-soundboard/raw/master/images/banner.png "DQ Soundboard Banner")
+
+
 # dragon-quest-soundboard
 A vanilla JavaScript soundboard featuring sounds from the first Dragon Quest(NES).
---
+++ script.js
@@ -0,0 +1,19 @@
+// soundboard logic
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; // skip if not a transform
+    this.classList.remove('playing');
+};
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return; // no key = no play!
+    audio.currentTime = 0; // rewind to start
+    audio.play();
+    key.classList.add('playing');
+};
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
